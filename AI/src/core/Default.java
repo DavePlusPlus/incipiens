2b92077dfaab11daa6c21d429e0ac638490ce0a2
DavePlusPlus <about.me/david.herrera>
2013-05-05 13:47:18 -0600

    Editing

--- AI/src/core/Default.java
+++ AI/src/core/Default.java
@@ -9,9 +9,37 @@ package core;
  */
 public class Default {
 
+	private String str1;
+	private String str2;
+
 	/**
-	 * 
+	 * @return the str1
 	 */
+	public String getStr1() {
+		return str1;
+	}
+
+	/**
+	 * @param str1 the str1 to set
+	 */
+	public void setStr1(String str1) {
+		this.str1 = str1;
+	}
+
+	/**
+	 * @return the str2
+	 */
+	public String getStr2() {
+		return str2;
+	}
+
+	/**
+	 * @param str2 the str2 to set
+	 */
+	public void setStr2(String str2) {
+		this.str2 = str2;
+	}
+
 	public Default() {
 		// TODO Auto-generated constructor stub
 	}
@@ -21,7 +49,7 @@ public class Default {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
