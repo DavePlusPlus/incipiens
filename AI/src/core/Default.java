9790fb85edc2e0149427f67db29121ca02d1c035
DavePlusPlus <about.me/david.herrera>
2013-05-05 13:18:35 -0600

    Editing

---
+++ AI/src/core/Default.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package core;
+
+/**
+ * @author David
+ *
+ */
+public class Default {
+
+	/**
+	 * 
+	 */
+	public Default() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
