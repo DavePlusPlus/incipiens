a78553fb047fda1b01b06daedc22f3f50c1ada36
DavePlusPlus <about.me/david.herrera>
2013-05-05 18:08:57 -0600

    Editing

---
+++ AI/src/core/Init.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package core;
+
+/**
+ * @author Dave (http://about.me/david.herrera)
+ *
+ */
+public class Init {
+
+	/**
+	 * 
+	 */
+	public Init() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
