1e11307a360255b5bbcd402c780f9903339ac22f
DavePlusPlus <about.me/david.herrera>
2013-05-26 10:46:50 -0600

    commit.

--- AI/src/draft/Sample.java
+++ AI/src/draft/Sample.java
@@ -7,11 +7,17 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Sample extends Application {
+	
+	static Text txt = new Text();
 	
 	public static void main(String[] args) throws ClassNotFoundException {
 		launch(args);
@@ -47,8 +53,15 @@ public class Sample extends Application {
 	@Override
 	public void start(Stage stage) {
 		stage.setTitle("Stage Title");
-		StackPane pane = new StackPane();
+		GridPane pane = new GridPane();
+		pane.setAlignment(Pos.CENTER);
+		pane.setHgap(10);
+		pane.setVgap(10);
+		pane.setPadding(new Insets(25, 25, 25, 25));
 		stage.setScene(new Scene(pane, 400, 300));
+		txt.setFill(Color.DEEPSKYBLUE);
+		txt.setText("Anything");
+		pane.add(txt, 0, 0);
 		stage.show();
 	}
 }
