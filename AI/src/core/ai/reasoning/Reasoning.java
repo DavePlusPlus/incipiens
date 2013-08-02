325dc7a2c828e220a76a568fc0b888d3c5fae11c
Dave <daveplusplus@live.com>
2013-08-02 11:49:08 -0600

    Settings.

---
+++ AI/src/core/ai/reasoning/Reasoning.java
@@ -0,0 +1,17 @@
+package core.ai.reasoning;
+
+public class Reasoning {
+
+	public Reasoning() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AI/src/core/data/Memory.java
@@ -0,0 +1,17 @@
+package core.data;
+
+public class Memory {
+
+	public Memory() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
