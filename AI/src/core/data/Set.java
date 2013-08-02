ecd1e0323dba79428a6cc4e03bc8d2eb8c14d7ab
Dave <daveplusplus@live.com>
2013-08-02 11:50:54 -0600

    Settings.

---
+++ AI/src/core/data/Set.java
@@ -0,0 +1,17 @@
+package core.data;
+
+public class Set {
+
+	public Set() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
