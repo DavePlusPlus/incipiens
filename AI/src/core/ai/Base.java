a354b795a2479788fe8abb205a34c65e8754824d
Dave <daveplusplus@live.com>
2013-08-02 11:01:06 -0600

    Develop.

---
+++ AI/src/core/ai/Base.java
@@ -0,0 +1,16 @@
+package core.ai;
+
+public class Base extends AbstBase implements InteBase {
+
+	public Base() {}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
