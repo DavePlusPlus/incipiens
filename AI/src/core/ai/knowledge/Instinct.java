90b3ee856f265abe9200caf9c21fd93ded4160c7
DavePlusPlus <about.me/david.herrera>
2013-08-05 20:52:58 -0600

    Working.

---
+++ AI/src/core/ai/knowledge/Instinct.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package core.ai.knowledge;
+
+/**
+ * @author Dave (http://about.me/david.herrera)
+ *
+ */
+public class Instinct {
+
+	public Instinct() {}
+
+}
---
+++ AI/src/core/ai/learning/Capture.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package core.ai.learning;
+
+/**
+ * @author Dave (http://about.me/david.herrera)
+ *
+ */
+public class Capture {
+
+	public Capture() {}
+
+}
